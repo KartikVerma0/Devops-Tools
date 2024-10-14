69acfccddb70b13cb46d1b180cb1fe7ea1f7f7e5
Kartik Verma <kartikvr20@gmail.com>
2024-10-14 16:17:18 +0530

    Added selectAll funds read only functionality

--- Select_All_Companies_Extension/script.js
+++ Select_All_Companies_Extension/script.js
@@ -1,12 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const selectAllButton = document.getElementById("selectall");
-  if (selectAllButton) {
-    selectAllButton.addEventListener("click", () => {
+  const selectAllButtonEdit = document.getElementById("selectalledit");
+  if (selectAllButtonEdit) {
+    selectAllButtonEdit.addEventListener("click", () => {
       // Query the active tab and inject the script
       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.scripting.executeScript({
           target: { tabId: tabs[0].id },
-          func: selectAllPermissions // This function will run in the context of the current tab
+          func: selectAllPermissionsEdit // This function will run in the context of the current tab
+        });
+      });
+    });
+  }
+
+  const selectAllButtonRead = document.getElementById("selectallread");
+  if (selectAllButtonRead) {
+    selectAllButtonRead.addEventListener("click", () => {
+      // Query the active tab and inject the script
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.scripting.executeScript({
+          target: { tabId: tabs[0].id },
+          func: selectAllPermissionsRead // This function will run in the context of the current tab
         });
       });
     });
@@ -14,7 +27,10 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 // The function to be executed in the context of the webpage
-function selectAllPermissions() {
+function selectAllPermissionsEdit() {
   document.querySelectorAll("em[mattooltip='Permission to edit']").forEach((ele) => ele.click());
 }
 
+function selectAllPermissionsRead(){
+  document.querySelectorAll("mat-checkbox").forEach((ele)=>{ele.children[0].children[0].children[0].click()})
+}
